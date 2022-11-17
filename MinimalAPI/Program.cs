ecc01784a4f2ce5ba51e268ebaac155458116626
feuvpi <fredvpg@gmail.com>
2022-11-16 21:00:43 -0300

    configuring swash.

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+
+app.MapGet("/user", () => new Cliente("Fred G", "fredg@fredg.com"));
+
+app.UseSwaggerUI();
+
 
 app.Run();
+
+public record Cliente(string Name, string Email);
